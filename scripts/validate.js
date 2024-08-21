f8ffd1c9aab4c5638cdbb02e4f9abce69164d3bb
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2024-08-21 20:37:14 -0300

    feat: finish validation functions

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,22 +1,34 @@
-const showInputError = (formElement, inputElement, inputElement.validationMessage)=> {
-  
-}
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}_error`);
 
-const isValid = (formElement, inputElement)=> {
+  inputElement.classList.add("popup__input_type_error");
+  errorElement.classList.add("popup__input-error_active");
+  errorElement.textContent = errorMessage;
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}_error`);
+
+  inputElement.classList.remove("popup__input_type_error");
+  errorElement.classList.add("popup__input-error_active");
+  errorElement.textContent = "";
+};
+
+const isValid = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
     hideInputError(formElement, inputElement);
   }
-}
+};
 
 const validateInputs = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
 
   inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', ()=> {
+    inputElement.addEventListener("input", () => {
       isValid(formElement, inputElement);
-    })
+    });
   });
 };
 
